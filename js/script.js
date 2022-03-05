ff0c462d0dfdfc799610b534c41cb76b32e22f14
Shedcape <shedcapeyt@gmail.com>
2022-03-05 11:54:09 +0100

    Refactored JS

--- js/script.js
+++ js/script.js
@@ -2,83 +2,67 @@ let trigger = false;
 let color = document.getElementById("colorpicker").value;
 let erase = false;
 let rainbowToggle = false;
-
 const generateGrid = (nr) => {
-    if (nr > 100 || nr < 16 || isNaN(nr)) {
-        return alert("Please choose a size between 16-100");
-    }
     for (let i = 0; i < nr; i++) {
         const row = generateRow(nr);
         document.querySelector('.gridcontainer').appendChild(row);
-    }
-}
+    };
+};
 const generateRow = (nr) => {
     const row = document.createElement('div');
     row.classList.add('row');
     for (let i = 0; i < nr; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.addEventListener('mouseenter', () => {
-            if (trigger && !erase && !rainbowToggle) {
-                square.style.backgroundColor = `${color}`;
-            } else if (trigger && erase) {
-                square.style.backgroundColor = 'white';
-            } else if (trigger && rainbowToggle) {
-                square.style.backgroundColor = rainbowColor();
-            }
+        square.addEventListener('mouseenter', (e) => {
+            square.style.backgroundColor = colorChange(e);
         });
-        square.addEventListener('click', () => {
-            if (!erase) {
-                square.style.backgroundColor = `${color}`;
-            } else if (erase) {
-                square.style.backgroundColor = `white`;
-            } else if (rainbowToggle) {
-                square.style.backgroundColor = rainbowColor();
-            }
-        })
-        square.addEventListener('touchmove', () => {
-            if (!erase) {
-                square.style.backgroundColor = `${color}`;
-            } else if (erase) {
-                square.style.backgroundColor = `white`;
-            } else if (rainbowToggle) {
-                square.style.backgroundColor = rainbowColor();
-            }
-        })
+        square.addEventListener('click', (e) => {
+            square.style.backgroundColor = colorChange(e);
+        });
         row.appendChild(square);
-    }
+    };
     return row;
-}
-const colorChange = () => {
-    if (!erase) {
-        this.style.backgroundColor = `${color}`;
-    } else {
-        this.style.backgroundColor = `white`;
-    }
-}
+};
+const colorChange = (e) => {
+    console.log(e);
+    if (e.type == 'click') {
+        if (!erase && !rainbowToggle) {
+            return `${color}`;
+        } else if (erase) {
+            return 'white';
+        } else if (rainbowToggle) {
+            return rainbowColor();
+        }
+    } else if (e.type == 'mouseenter') {
+        if (trigger && !erase && !rainbowToggle) {
+            return `${color}`;
+        } else if (trigger && erase) {
+            return 'white';
+        } else if (trigger && rainbowToggle) {
+            return rainbowColor();
+        };
+    };
+};
 document.querySelector('#reset').addEventListener('click', () => {
     const container = document.querySelector('.gridcontainer');
     while (container.lastElementChild) {
         container.removeChild(container.lastElementChild);
     }
     generateGrid(document.getElementById("sizeSlider").value);
-})
+});
 document.addEventListener('mousedown', () => {
     trigger = true;
 });
 document.addEventListener('mouseup', () => {
     trigger = false;
-})
-document.addEventListener('touchmove', () => {
-    trigger = true;
-})
+});
 const rainbowColor = () => {
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
-}
-
+};
 const rangeSlider = document.getElementById("sizeSlider");
 rangeSlider.addEventListener('input', () => {
     document.getElementById("sliderindicator").textContent = rangeSlider.value + " x " + rangeSlider.value;
@@ -109,5 +93,4 @@ rainbow.addEventListener('click', () => {
         rainbow.classList.add('selected');
     };
 })
-
 generateGrid(16);
