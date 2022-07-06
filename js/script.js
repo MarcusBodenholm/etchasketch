c6ef482a2701c7ed35784ea129d9a22e64feb593
Shedcape <shedcapeyt@gmail.com>
2022-07-06 21:56:23 +0200

    Had to reverse some changes from previous version due to a bug that caused severe performance issues

--- js/script.js
+++ js/script.js
@@ -3,83 +3,17 @@ let color = document.getElementById('colorpicker').value;
 let erase = false;
 let rainbowToggle = false;
 
-document.addEventListener('mousedown', () => {
-  trigger = true;
-});
-
-document.addEventListener('mouseup', () => {
-  trigger = false;
-});
-
-const rainbowColor = () => {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-  return `rgb(${r}, ${g}, ${b})`;
-};
-
-function colorChange(e) {
-  if (e.type === 'click') {
-    if (!erase && !rainbowToggle) {
-      return `${color}`;
-    }
-    if (erase) {
-      return 'white';
-    }
-    if (rainbowToggle) {
-      return rainbowColor();
-    }
-  } else if (e.type === 'mouseenter') {
-    if (trigger && !erase && !rainbowToggle) {
-      return `${color}`;
-    }
-    if (trigger && erase) {
-      return 'white';
-    }
-    if (trigger && rainbowToggle) {
-      return rainbowColor();
-    }
-  }
+function generateGrid(nr) {
+  for (let i = 0; i < nr; i++) {
+    const row = generateRow(nr);
+    document.querySelector('.gridcontainer').appendChild(row);
+  };
 }
 
-const rangeSlider = document.getElementById('sizeSlider');
-rangeSlider.addEventListener('input', () => {
-  document.getElementById('sliderindicator').textContent = `${rangeSlider.value} x ${rangeSlider.value}`;
-});
-
-const colorPicker = document.getElementById('colorpicker');
-colorPicker.addEventListener('input', () => {
-  if (!erase) {
-    color = colorPicker.value;
-  }
-});
-
-const eraser = document.getElementById('eraser');
-eraser.addEventListener('click', () => {
-  if (erase) {
-    erase = false;
-    eraser.classList.remove('selected');
-  } else {
-    erase = true;
-    eraser.classList.add('selected');
-  }
-});
-
-const rainbow = document.getElementById('rainbow');
-rainbow.addEventListener('click', () => {
-  if (rainbowToggle) {
-    rainbowToggle = false;
-    rainbow.classList.remove('selected');
-  } else {
-    rainbowToggle = true;
-    rainbow.classList.add('selected');
-  }
-});
-
-const generateRow = (nr) => {
+function generateRow(nr) {
   const row = document.createElement('div');
   row.classList.add('row');
-  for (let i = 0; i < nr; i + 1) {
+  for (let i = 0; i < nr; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     square.addEventListener('mouseenter', (e) => {
@@ -89,16 +23,30 @@ const generateRow = (nr) => {
       square.style.backgroundColor = colorChange(e);
     });
     row.appendChild(square);
-  }
+  };
   return row;
-};
+}
 
-const generateGrid = (nr) => {
-  for (let i = 0; i < nr; i + 1) {
-    const row = generateRow(nr);
-    document.querySelector('.gridcontainer').appendChild(row);
-  }
-};
+function colorChange(e) {
+  console.log(e);
+  if (e.type == 'click') {
+    if (!erase && !rainbowToggle) {
+      return `${color}`;
+    } else if (erase) {
+      return 'white';
+    } else if (rainbowToggle) {
+      return rainbowColor();
+    }
+  } else if (e.type == 'mouseenter') {
+    if (trigger && !erase && !rainbowToggle) {
+      return `${color}`;
+    } else if (trigger && erase) {
+      return 'white';
+    } else if (trigger && rainbowToggle) {
+      return rainbowColor();
+    };
+  };
+}
 
 document.querySelector('#reset').addEventListener('click', () => {
   const container = document.querySelector('.gridcontainer');
@@ -107,5 +55,54 @@ document.querySelector('#reset').addEventListener('click', () => {
   }
   generateGrid(document.getElementById('sizeSlider').value);
 });
+
+document.addEventListener('mousedown', () => {
+  trigger = true;
+});
+
+document.addEventListener('mouseup', () => {
+  trigger = false;
+});
+
+function rainbowColor() {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const rangeSlider = document.getElementById("sizeSlider");
+rangeSlider.addEventListener('input', () => {
+  document.getElementById("sliderindicator").textContent = `${rangeSlider.value} x ${rangeSlider.value}`;
+})
+
+const colorPicker = document.getElementById("colorpicker");
+colorPicker.addEventListener('input', () => {
+  if (!erase) {
+    color = colorPicker.value;
+  }
+})
+
+const eraser = document.getElementById('eraser');
+eraser.addEventListener('click', () => {
+  if (erase) {
+    erase = false;
+    eraser.classList.remove('selected');
+  } else {
+    erase = true;
+    eraser.classList.add('selected');
+  };
+})
+
+const rainbow = document.getElementById('rainbow');
+rainbow.addEventListener('click', () => {
+  if (rainbowToggle) {
+    rainbowToggle = false;
+    rainbow.classList.remove('selected');
+  } else {
+    rainbowToggle = true;
+    rainbow.classList.add('selected');
+  };
+})
 
 generateGrid(16);
